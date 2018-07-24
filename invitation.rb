d7fb8997ac99d729a3a0cf1c83bbb391f23d63cc
hanam0822 <hanam0822@gmail.com>
2018-07-24 13:29:48 +0000

    Done.

--- invitation.rb
+++ invitation.rb
@@ -1,3 +1,9 @@
-# Code your prompts here!
+guest_name = "Tom"
+party_name = "Ultimate Birthday Bash"
+date  = "August 4th" 
+time = "3pm"
+host_name = "Hana"
 
-# Try starting out with puts'ing a string.
+puts "Dear #{guest_name}, "
+puts "You are invited to the #{party_name} on #{date} at #{time}. We hope you can attend!
+Sincerely, #{host_name}"
